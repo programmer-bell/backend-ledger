de58e59ab33852063ff6f9a7b7eeb6304e4f21b1
Badhan Sarkar <badhansarkar164@gmail.com>
2026-02-11 21:44:47 +0530

    create outes, use inbuilt middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
 const express = require("express")
+const cookieParser = require("cookie-parser")
 
+
+// Routes required
+const authRouter = require("./routes/auth.route")
+
+// Initialize the express
 const app = express()
+
+// Use Inbuilt middleware
+app.use(express.json())
+app.use(cookieParser())
+
+
+// Use Routes
+app.use("/api/auth",authRouter)
+
+
 
 
 module.exports = app
