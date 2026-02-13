b1571e06d573db00bbe01b7982202b25581f257c
Badhan Sarkar <badhansarkar164@gmail.com>
2026-02-13 17:44:50 +0530

    good code wruting alog side commands on server.js

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db")
 
+
+// Database Connection
 connectDB()
 
+// Port use
 const PORT = process.env.PORT;
 
 
 
-
-
+// Run the Server
 app.listen(PORT, () => {
 
     console.log(`Server is running at: http://localhost:${PORT}`);
